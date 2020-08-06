26c9f8ef1fe99395dd69c5e3755e7fced006470f
EdieAprovita <edieveg316@gmail.com>
2020-08-05 20:12:18 -0500

    Basic file

---
+++ salida.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+//types
+var myString = 'Hello world';
+myString = 22 + '';
+var myNumber = 22;
+var myBool = false;
+var myVariable = 'hello';
+myVariable = false;
+//Arrays
+var stringArray = ['item', 'item2', 'item3', ''];
+var numberArray = [1, 2, 3, 4, 5];
+var booleanArray = [true, false, false, true];
+var anyArray = [1, 2, 'Bye', true];
+document.write();
---
+++ salida.ts
@@ -0,0 +1,21 @@
+console.log('Hello World!');
+
+//types
+
+var myString: string = 'Hello world';
+myString = 22 + '';
+
+var myNumber = 22;
+var myBool: boolean = false;
+
+var myVariable: any = 'hello';
+myVariable = false;
+
+//Arrays
+
+var stringArray: string[] = ['item', 'item2', 'item3', ''];
+var numberArray: number[] = [1, 2, 3, 4, 5];
+var booleanArray: boolean[] = [true, false, false, true];
+var anyArray: any[] = [1, 2, 'Bye', true];
+
+document.write();
